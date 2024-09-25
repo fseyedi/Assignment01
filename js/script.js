16f1fb1d54534b7661b9872839943bc975f0f111
Faraz <fseyedi@student.sdccd.edu>
2024-09-25 05:55:40 -0700

    Assiggnment1

---
+++ js/script.js
@@ -0,0 +1,157 @@
+// Exercise 1
+let someMonth
+function theMonth(){}
+let currentMonth
+let summerMonth
+let myLibraryFunction
+
+// Exercise 2
+126 //numeric literal expression
+'test' //a string literal expression
+true //a Boolean literal expression
+null // a null literal expression
+
+// Exercise 3
+let myString = 'test'+' 2'
+let myNumber = 3 + 15
+
+// Exercise 4
+
+    //Camel Casing
+let firstName
+let lastName
+let address
+let city
+let state 
+let zipCode
+let yourAge
+let referralSource
+let mayWeContactYou
+
+    //Hungarian Notation
+let strFirstName
+let strLastName
+let strAddress
+let strCity
+let strState 
+let intZipCode
+let intYourAge
+let strReferralSource
+let blnMayWeContactYou
+
+// Exercise 5
+strFirstName='Faraz'
+let strFirstName2='Faraz'
+let strFirstName3 ='Faraz' , strLastName1='Se'
+
+// Exercise 6
+let fN='Faraz'
+let selectNumber = 12
+
+console.info(`${fN} ${selectNumber}`)
+
+// Exercise 7
+
+mayWeContactYou = true
+firstName = 'Faraz'
+
+let firstVar =  mayWeContactYou+firstName
+console.info(`${firstVar}`)
+
+yourAge = 100
+let secVar = yourAge + mayWeContactYou
+console.info(`${secVar}`)
+
+// Exercise 8
+
+let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
+console.log(someString)
+
+// Exercise 9
+
+let x = null
+console.info(`${x}`) //null value
+
+let y
+console.info(`${y}`) //undefined value
+
+// Exercise 10
+
+console.info(`${typeof('Faraz')}`) //string
+console.info(`${typeof(100)}`) //number
+console.info(`${typeof(true)}`) //Boolean
+console.info(`${typeof([1,2,3])}`) //object
+console.info(`${typeof(yzzz)}`) //undefined
+
+// Exercise 11
+
+window.alert('Hello'+' Faraz'+', welcome to the JavaScript class!')
+
+// Exercise 12
+
+let myName = 'Faraz'
+
+window.alert('Hello '+myName+', welcome to the JavaScript class!')
+
+// Exercise 13
+
+let course = 'JavaScript'
+window.alert(`${course}`)
+
+// Exercise 14
+
+window.alert('Hello '+myName+'. \nwelcome to the JavaScript class!')
+
+// Exercise 15
+
+myName = window.prompt('please enter your name :')
+window.alert('Hello '+myName+'. \nwelcome to the JavaScript class!')
+
+// Exercise 16
+
+course = window.prompt('please enter class you taking :')
+window.alert('Hello '+myName+'. \nwelcome to the '+course+' class!')
+
+// Exercise 17
+
+let x1=10, y1=20
+let total = x1+y1
+console.info(total)
+
+// Exercise 18
+
+let x2=20
+
+console.info(x2+=20)
+
+// Exercise 19
+
+let x3=20
+console.info(x3*=5)
+
+// Exercise 20
+
+let x4=20
+x4%=3
+x4/=1
+console.info(x4)
+
+
+// Exercise 21
+
+x1= 30
+y1= 40
+
+console.info(x1!=y1 && x1<=y1)
+
+// Exercise 22
+
+x1=30
+let y2='30'
+
+console.log(x1===y2 || x1!=y2)
+
+
+
+
+
